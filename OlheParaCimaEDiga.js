e5122beb2bb39a30d97326d9e4a12a3c998befdf
Andre Soares <andresoaresti@gmail.com>
2017-12-15 02:44:40 -0200

    Resolução do dojo da CSU

---
+++ OlheParaCimaEDiga.js
@@ -0,0 +1,41 @@
+
+function OlheParaCimaEDiga(sequenciaAtual, quantidadeDeLinhas)
+{
+	quantidadeDeLinhas--;
+
+	sequenciaAtual = sequenciaAtual.split('');
+	var sequenciaSeguinte = '';
+	var quantidadeDeRepeticoes = 0;
+
+	var indexDePosicoes = 0;
+
+	if (sequenciaAtual.length == 1) {
+		console.log(sequenciaAtual[0]);
+	}
+
+	for(var i = 0; i < sequenciaAtual.length; i++){
+	
+		var elementoAtual = sequenciaAtual[i]; // 1
+
+		while(elementoAtual == sequenciaAtual[indexDePosicoes]){
+			
+			indexDePosicoes++; //1
+			quantidadeDeRepeticoes++;	
+		}
+		if (quantidadeDeRepeticoes > 0)
+			sequenciaSeguinte = sequenciaSeguinte + quantidadeDeRepeticoes  +  elementoAtual ;
+
+		quantidadeDeRepeticoes = 0;
+	}
+
+
+	
+	if(quantidadeDeLinhas > 0){
+		console.log(sequenciaSeguinte);
+		OlheParaCimaEDiga(sequenciaSeguinte, quantidadeDeLinhas);
+	}
+
+
+}
+
+OlheParaCimaEDiga('X', 5);
